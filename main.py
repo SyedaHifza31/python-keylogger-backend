b3f350885e9c9a5ae97c11d684b3e18a55cd7aae
SyedaHifza31 <hifzaali480@gmail.com>
2026-02-02 14:43:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,14 @@ with keyboard.Listener(
     on_press=on_press,
     on_release=on_release) as listener:
     listener.join()
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
